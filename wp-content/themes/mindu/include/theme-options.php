8d5d712f03560bb4caac5f8733dfc47a9462b9c2
kowshar-ahmed <kowshar11111@gmail.com>
2026-05-03 16:17:17 +0600

    change

--- wp-content/themes/mindu/include/theme-options.php
+++ wp-content/themes/mindu/include/theme-options.php
@@ -34,20 +34,14 @@ Redux::set_section(
                     'content' => 'If you remove the text, the button will be hidden.',
                 )
             ),
-        )
             array(
                 'id'       => 'header-button-url',
                 'type'     => 'text',
                 'title'    => esc_html__('Button URL', 'mindu'),
                 'desc'     => esc_html__('Button URL here.', 'mindu'),
                 'subtitle' => esc_html__('Button URL here.', 'mindu'),
-            ),
-            array(
-                'id'       => 'header-button-url',
-                'type'     => 'text',
-                'title'    => esc_html__('Button URL', 'mindu'),
-                'desc'     => esc_html__('Button URL here.', 'mindu'),
-                'subtitle' => esc_html__('Button URL here.', 'mindu'),
+
             )
+        )
     )
 );
