a0710887daf879f1e91654d7a4dd3a33c9312ec3
kowshar-ahmed <kowshar11111@gmail.com>
2026-05-20 17:46:44 +0600

    Blog Start with Post Format Class 13 Start

--- wp-content/themes/mindu/index.php
+++ wp-content/themes/mindu/index.php
@@ -4,7 +4,187 @@
 
 
 
+<div class="tp-postbox-area pt-140 pb-100">
+    <div class="container container-1324">
+        <div class="row">
+            <div class="col-lg-8">
+                <div class="postbox-wrapper mr-50 mb-40">
+                    
+                    <article class="postbox-item mb-45">
+                        <div class="tp-blog-item mb-40">
+                            <div class="tp-blog-thumb overflow-hidden mb-35">
+                                <a href="blog-details.html">
+                                    <img class="w-100" src="assets/img/blog/postbox/thumb.jpg" alt="Mindu Blog Article - Expert Tips for Effective Online Education and Learning Success">
+                                </a>
+                            </div>
+                            <div class="tp-blog-content">
+                                <div class="tp-blog-meta-wrap d-flex flex-wrap gap-1 mb-15">
+                                    <span class="tp-blog-meta fw-500 d-inline-block">
+                                        <span>
+                                            <svg width="13" height="15" viewBox="0 0 13 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M8.54969 4H3.64969M6.44899 6.8H3.649" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
+                                                <path d="M11.7 14.5H1.9C1.1268 14.5 0.5 13.8732 0.5 13.1M0.5 13.1C0.5 12.3268 1.1268 11.7 1.9 11.7H11.7V3.3C11.7 1.98007 11.7 1.3201 11.2899 0.910053C10.8799 0.5 10.2199 0.5 8.9 0.5H4.7C2.7201 0.5 1.73015 0.5 1.11508 1.11508C0.5 1.73015 0.5 2.7201 0.5 4.7V13.1Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
+                                                <path d="M11.3495 11.6992C11.3495 11.6992 10.6495 12.2332 10.6495 13.0992C10.6495 13.9653 11.3495 14.4992 11.3495 14.4992" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
+                                            </svg>
+                                        </span>
+                                        Education
+                                    </span>
+                                    <span class="tp-blog-meta fw-500 d-inline-block">
+                                        <span>
+                                            <svg width="14" height="15" viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M9.69231 0.5V3.30004M4.03543 0.5V3.30004" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
+                                                <path d="M7.5711 1.89941H6.15688C3.4902 1.89941 2.15686 1.89941 1.32843 2.71952C0.5 3.53964 0.5 4.85959 0.5 7.49949V8.89951C0.5 11.5394 0.5 12.8594 1.32843 13.6795C2.15686 14.4996 3.4902 14.4996 6.15688 14.4996H7.5711C10.2378 14.4996 11.5711 14.4996 12.3995 13.6795C13.228 12.8594 13.228 11.5394 13.228 8.89951V7.49949C13.228 4.85959 13.228 3.53964 12.3995 2.71952C11.5711 1.89941 10.2378 1.89941 7.5711 1.89941Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
+                                                <path d="M0.5 6.09961H13.228" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
+                                            </svg>
+                                        </span>
+                                        August 6,2026
+                                    </span>
+                                </div>
+                                <h3 class="tp-blog-title fs-32 fw-600 mb-15">
+                                    <a href="blog-details.html">Creating inclusive classrooms where<br> every student truly belongs.</a>
+                                </h3>
+                                <p class="fw-500">We foster a supportive learning environment where diversity is celebrated<br> every voice is respected and each student feels valued grow.</p>
+                            </div>
+                        </div>
+                    </article>
 
+                    <div class="pt-5">
+                        <nav class="navigation pagination wp-pagination">
+                            <div class="nav-links">
+                                <a class="prev page-numbers" href="#">
+                                    <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M10.75 5.75H0.75" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                        <path d="M5.75 10.75L0.75 5.75L5.75 0.75" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                    </svg>
+                                </a>
+                                <span class="page-numbers current" aria-current="page">01</span>
+                                <a class="page-numbers" href="#">02</a>
+                                <a class="page-numbers" href="#">03</a>
+                                <a class="page-numbers" href="#">04</a>
+                                <a class="next page-numbers" href="#">
+                                    <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                       <path d="M0.75 5.75H10.75" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                        <path d="M5.75 10.75L10.75 5.75L5.75 0.75" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                    </svg>
+                                </a>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="tp-sidebar-wrapper">
+                    <div class="tp-sidebar-search p-relative mb-40">
+                        <form action="#">
+                            <input class="tp-input" type="text" placeholder="Search ...">
+                            <button class="tp-sidebar-search-btn">
+                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                     <path d="M6.97222 13.1944C10.4087 13.1944 13.1944 10.4087 13.1944 6.97222C13.1944 3.53578 10.4087 0.75 6.97222 0.75C3.53578 0.75 0.75 3.53578 0.75 6.97222C0.75 10.4087 3.53578 13.1944 6.97222 13.1944Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                    <path d="M14.75 14.7502L11.3667 11.3669" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+                            </button>
+                        </form>
+                    </div>
+                    <div class="tp-sidebar-service-list tp-sidebar-border pb-30 mb-35">
+                        <h3 class="tp-sidebar-service-title mb-20 tp-ff-body">Categories</h3>
+                        <ul>
+                            <li>
+                                <a href="service-details.html">Articles
+                                    <span>(8)</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="service-details.html">Business
+                                    <span>(4)</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="service-details.html">Family & Divorce
+                                    <span>(3)</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="service-details.html">Web Design
+                                    <span>(2)</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="service-details.html">Software
+                                    <span>(8)</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="service-details.html">Video
+                                    <span>(1)</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="tp-sidebar-widget tp-sidebar-border pb-30 mb-35">
+                        <h3 class="tp-sidebar-service-title mb-20 tp-ff-body">Recent Posts</h3>
+                        <div class="tp-recent-post-content d-flex align-items-center">
+                            <div class="tp-recent-post-thumb mr-15">
+                                <img src="assets/img/blog/blog.jpg" alt="">
+                            </div>
+                            <div>
+                                <span class="tp-recent-post-span">UI Desige</span>
+                                <h2 class="tp-recent-post-title"><a href="blog-details.html">Google without having to hire an SEO Expert.</a></h2>
+                                <div class="tp-recent-post-tag">
+                                    <span>14 April, 2024</span>
+                                    <span>Minute</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tp-recent-post-content d-flex align-items-center">
+                            <div class="tp-recent-post-thumb mr-15">
+                                <img src="assets/img/blog/blog-3.jpg" alt="">
+                            </div>
+                            <div>
+                                <span class="tp-recent-post-span grey">Career</span>
+                                <h2 class="tp-recent-post-title"><a href="blog-details.html">Being good cyber citizens in a digital world.</a></h2>
+                                <div class="tp-recent-post-tag">
+                                    <span>14 April, 2024</span>
+                                    <span>Minute</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tp-recent-post-content d-flex align-items-center">
+                            <div class="tp-recent-post-thumb mr-15">
+                                <img src="assets/img/blog/blog-2.jpg" alt="">
+                            </div>
+                            <div>
+                                <span class="tp-recent-post-span yellow">Software</span>
+                                <h2 class="tp-recent-post-title"><a href="blog-details.html">The single biggest reason why startups succeed.</a></h2>
+                                <div class="tp-recent-post-tag">
+                                    <span>14 April, 2024</span>
+                                    <span>Minute</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tp-sidebar-widget tp-sidebar-border pb-30 mb-50">
+                        <div class="tp-sidebar-content">
+                            <h3 class="tp-sidebar-service-title mb-20 tp-ff-body">Tag</h3>
+                            <div class="tagcloud">
+                                <a href="#">News</a>
+                                <a href="#">Counseling</a>
+                                <a href="#">Career</a>
+                                <a href="#">Software</a>
+                                <a href="#">Development</a>
+                                <a href="#">Merket</a>
+                                <a href="#">Life</a>
+                                <a href="#">Research</a>
+                                <a href="#">Research</a>
+                                <a href="#">UI Desige</a>
+                                <a href="#">Team</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 
