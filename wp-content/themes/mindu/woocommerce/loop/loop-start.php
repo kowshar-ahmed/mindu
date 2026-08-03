dff477739b5a3efdcf04e3383833f6df3fc398b2
Kowshar Ahmed <your-email@example.com>
2026-08-03 07:34:05 +0600

    WooCommerce Start Class 28 Part 02 Done

--- wp-content/themes/mindu/woocommerce/loop/loop-start.php
+++ wp-content/themes/mindu/woocommerce/loop/loop-start.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Product Loop Start
  *
@@ -15,8 +16,8 @@
  * @version     3.3.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (! defined('ABSPATH')) {
 	exit;
 }
 ?>
-<ul class="products columns-<?php echo esc_attr( wc_get_loop_prop( 'columns' ) ); ?>">
+<div class="row row-cols-md-2 row-cols-lg-3 row-cols-xl-<?php echo esc_attr(wc_get_loop_prop('columns')); ?> ">
